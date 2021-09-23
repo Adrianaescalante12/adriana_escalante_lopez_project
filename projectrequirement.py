bbcbc59577000fb88a53e5034687e112775c1f52
Adrianaescalante12 <aescala097@gmail.com>
2021-09-23 17:13:05 +0000

    half done projectreq doc

--- projectrequirement.py
+++ projectrequirement.py
@@ -2,34 +2,31 @@
 
 ### Overview
 
-A app where users can rate and review movies, similar to https://letterboxd.com/
+A news aggregator app where users can easily access government related articles.
 
 ### Technologies required (besides typical Hackbright tech stack)
 
-- The Movie DB API: https://developers.themoviedb.org/3/getting-started/introduction
+- News API: https://newsapi.org/docs/get-started (Similar interface as feeder.co)
 
 ### Data
 
-- User email, password
-- Movie title, description
-  - Maybe more metadata like genre, cast and crew...
-- Ratings
-- Reviews/comments
+- 
 
 ### Roadmap
 
 #### MVP
 
-- Users can view a list of movies and details about each movie
-- Users can login
-- Users can add a review to a movie
-- Users can rate a movie between 1–5
+- Users can view a list of government related articles and details about the article
+- Users can search filter time-frame and news company (language set to english by default)
+- Users can search by keyword
+- Users can star/bookmark articles to read later with its own dedicated section to access
+
+
 
 #### 2.0
 
-- Get movie posters
-- Make the UI super slick
-- Share on Twitter?
+- Ability to comment on articles and create a discussion with other users
+- Ability to bucket starred/bookmarked articles by a user-defined folder
 
 #### 3.0
 
