f2f5765f715df8ca585e6f90da857168c67807e8
Adrianaescalante12 <aescala097@gmail.com>
2021-09-23 16:43:36 +0000

    initial commit, includes just project requirement doc

---
+++ projectrequirement.py
@@ -0,0 +1,36 @@
+## Project Proposal
+
+### Overview
+
+A app where users can rate and review movies, similar to https://letterboxd.com/
+
+### Technologies required (besides typical Hackbright tech stack)
+
+- The Movie DB API: https://developers.themoviedb.org/3/getting-started/introduction
+
+### Data
+
+- User email, password
+- Movie title, description
+  - Maybe more metadata like genre, cast and crew...
+- Ratings
+- Reviews/comments
+
+### Roadmap
+
+#### MVP
+
+- Users can view a list of movies and details about each movie
+- Users can login
+- Users can add a review to a movie
+- Users can rate a movie between 1–5
+
+#### 2.0
+
+- Get movie posters
+- Make the UI super slick
+- Share on Twitter?
+
+#### 3.0
+
+- React???
