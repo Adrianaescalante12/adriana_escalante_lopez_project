83b9f69d23efa3e0983b802e5890d06389c99bee
Adrianaescalante12 <aescala097@gmail.com>
2021-09-24 02:19:44 +0000

    Final project requirement for advisor approval

--- projectrequirement.py
+++ projectrequirement.py
@@ -9,8 +9,12 @@ A news aggregator app where users can easily access government related articles.
 - News API: https://newsapi.org/docs/get-started (Similar interface as feeder.co)
 
 ### Data
+- default government keywords to filter API on (1.0)
+- usernames and passwords (1.0)
+- starred/bookmarked articles (1.0)
+- comments on articles (3.0)
+- bucketed starred/bookmarked articles (3.0)
 
-- 
 
 ### Roadmap
 
@@ -18,16 +22,14 @@ A news aggregator app where users can easily access government related articles.
 
 - Users can view a list of government related articles and details about the article
 - Users can search filter time-frame and news company (language set to english by default)
+
+#### 2.0
 - Users can search by keyword
 - Users can star/bookmark articles to read later with its own dedicated section to access
 
-
-
-#### 2.0
+#### 3.0
 
 - Ability to comment on articles and create a discussion with other users
 - Ability to bucket starred/bookmarked articles by a user-defined folder
 
-#### 3.0
-
-- React???
+### Notes
